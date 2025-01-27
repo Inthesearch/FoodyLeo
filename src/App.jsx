39c860e119aa0d1f4951ef03a2aa8eb96ded3c24
Inthesearch <goyalr5044@gmail.com>
2025-01-27 08:42:48 -0500

    Cart component completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import Cart from "./components/Cart";
 import Header from "./components/header";
 import Meals from "./components/Meals";
 import { CartContextProvider } from "./store/CartContext";
+import { UserProgressContextProvider } from "./store/UserProgressContext";
+
 function App() {
   return (
     <CartContextProvider>
-      <Header></Header>
-      <Meals />
+      <UserProgressContextProvider>
+        <Header></Header>
+        <Meals />
+        <Cart />
+      </UserProgressContextProvider>
     </CartContextProvider>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,19 @@ import logoImg from "../assets/logo.jpg";
 import Button from "./UI/Button";
 import CartContext from "../store/CartContext";
 import { useContext } from "react";
+import UserProgressContext from "../store/UserProgressContext";
 
 export default function Header() {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
   const noOfTotalItems = cartCtx.items.reduce(
     (total, item) => total + item.quantity,
     0
   );
+
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
 
   return (
     <header id="main-header">
@@ -17,7 +23,9 @@ export default function Header() {
         <h1>Foody Panda</h1>
       </div>
       <nav>
-        <Button textOnly>Cart ({noOfTotalItems})</Button>
+        <Button textOnly onClick={handleShowCart}>
+          Cart ({noOfTotalItems})
+        </Button>
       </nav>
     </header>
   );
